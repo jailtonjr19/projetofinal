84f49f99014ba5432c4326c58afd0b2a14778374
jailtonjr19 <juniorplay2018@gmail.com>
2021-12-27 15:40:29 -0300

    Arquivos atualizados após a conexão com o Banco de dados

--- src/main/java/projeto/cgra/controllers/templates/cadastroCaprinoController.java
+++ src/main/java/projeto/cgra/controllers/templates/cadastroCaprinoController.java
@@ -3,11 +3,11 @@ package projeto.cgra.controllers.templates;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class cadastroCaprinoController {
 	@RequestMapping("/cadastroCaprino")
 	public String cadastroCaprino(){
 		return "cadastroCaprino.html";
 	}
-
 }
